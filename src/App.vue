596ce756241cef2a8a30a5aa5cebe1207e73087a
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-05-07 12:49:26 +0500

    uzgarish

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+   <div class="container p-4">
+     
+      <h1>counter:{{counter}} </h1>
+      <div class="d-flex">
+        <button v-on:click='go' class="btn btn-success ">Add</button>
+        <button v-on:click='btn' class="btn btn-danger">delete</button>
+      </div>
+    </div>
+  
+</template>
+
+<script>
+   export default{
+    data(){
+      return{
+        counter:0
+      }
+    },
+    methods:{
+      go(){
+        this.counter += 1
+      },
+      btn(){
+        this.counter -=1
+      },
+    },
+    
+   }
+</script>
+
+<style>
+
+</style>
